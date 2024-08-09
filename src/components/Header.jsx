b568f409ed25985cc0845b2264eedc57e77d3c48
sovanchitra <chivsovanchetra@gmail.com>
2024-08-09 21:14:02 +0700

    feat-homepage: create navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,213 @@
-import { NavLink } from "react-router-dom";
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Logo from "../assets/logo.png";
+import { Stack } from "@mui/material";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+// import theme from "../theme";
+// import { ThemeProvider } from "styled-components";
 
-const HEADER_DATA = [
-  { page: "My Learning", path: "mylearning" },
-  { page: "Marketplace", path: "marketplace" },
-  { page: "Become a member", path: "" },
-  { page: "AgAI", path: "agai" },
-  { page: "Wishlist", path: "wishlist" },
-  { page: "Cart", path: "cart" },
-  { page: "Login", path: "login" },
-  { page: "Course detail", path: "courses/:coursesId" },
-];
+// const drawerWidth = 240;
+// const navItems = ['Home', 'About', 'Contact'];
 
-function Navigation() {
+// function DrawerAppBar(props) {
+//   const { window } = props;
+//   const [mobileOpen, setMobileOpen] = React.useState(false);
+
+//   const handleDrawerToggle = () => {
+//     setMobileOpen((prevState) => !prevState);
+//   };
+
+//   const drawer = (
+//     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+//       <Typography variant="h6" sx={{ my: 2 }}>
+//         MUI
+//       </Typography>
+//       <Divider />
+//       <List>
+//         {navItems.map((item) => (
+//           <ListItem key={item} disablePadding>
+//             <ListItemButton sx={{ textAlign: 'center' }}>
+//               <ListItemText primary={item} />
+//             </ListItemButton>
+//           </ListItem>
+//         ))}
+//       </List>
+//     </Box>
+//   );
+
+//   const container = window !== undefined ? () => window().document.body : undefined;
+
+//   return (
+//     <Box sx={{ display: 'flex' }}>
+//       <CssBaseline />
+//       <AppBar component="nav" >
+//         <Toolbar>
+//           <IconButton
+//             color="inherit"
+//             aria-label="open drawer"
+//             edge="start"
+//             onClick={handleDrawerToggle}
+//             sx={{ mr: 2, display: { sm: 'none' } }}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//           <Typography
+//             variant="h6"
+//             component="div"
+//             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+//           >
+//             AgTeach
+//           </Typography>
+//           <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+//             {navItems.map((item) => (
+//               <Button key={item} sx={{ color: '#fff' }}>
+//                 {item}
+//               </Button>
+//             ))}
+//           </Box>
+//         </Toolbar>
+//       </AppBar>
+//       <nav>
+//         <Drawer
+//           container={container}
+//           variant="temporary"
+//           open={mobileOpen}
+//           onClose={handleDrawerToggle}
+//           ModalProps={{
+//             keepMounted: true, // Better open performance on mobile.
+//           }}
+//           sx={{
+//             display: { xs: 'block', sm: 'none' },
+//             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+//           }}
+//         >
+//           {drawer}
+//         </Drawer>
+//       </nav>
+//       <Box component="main" sx={{ p: 3 }}>
+//         <Toolbar />
+//         <Typography>
+//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde
+//           fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam,
+//           aliquam dolore excepturi quae. Distinctio enim at eligendi perferendis in
+//           cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem,
+//           at ab sequi qui modi delectus quia corrupti alias distinctio nostrum.
+//           Minima ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed
+//           numquam quibusdam at officia sapiente porro maxime corrupti perspiciatis
+//           asperiores, exercitationem eius nostrum consequuntur iure aliquam itaque,
+//           assumenda et! Quibusdam temporibus beatae doloremque voluptatum doloribus
+//           soluta accusamus porro reprehenderit eos inventore facere, fugit, molestiae
+//           ab officiis illo voluptates recusandae. Vel dolor nobis eius, ratione atque
+//           soluta, aliquam fugit qui iste architecto perspiciatis. Nobis, voluptatem!
+//           Cumque, eligendi unde aliquid minus quis sit debitis obcaecati error,
+//           delectus quo eius exercitationem tempore. Delectus sapiente, provident
+//           corporis dolorum quibusdam aut beatae repellendus est labore quisquam
+//           praesentium repudiandae non vel laboriosam quo ab perferendis velit ipsa
+//           deleniti modi! Ipsam, illo quod. Nesciunt commodi nihil corrupti cum non
+//           fugiat praesentium doloremque architecto laborum aliquid. Quae, maxime
+//           recusandae? Eveniet dolore molestiae dicta blanditiis est expedita eius
+//           debitis cupiditate porro sed aspernatur quidem, repellat nihil quasi
+//           praesentium quia eos, quibusdam provident. Incidunt tempore vel placeat
+//           voluptate iure labore, repellendus beatae quia unde est aliquid dolor
+//           molestias libero. Reiciendis similique exercitationem consequatur, nobis
+//           placeat illo laudantium! Enim perferendis nulla soluta magni error,
+//           provident repellat similique cupiditate ipsam, et tempore cumque quod! Qui,
+//           iure suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
+//           Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore commodi
+//           reprehenderit rerum reiciendis! Quidem alias repudiandae eaque eveniet
+//           cumque nihil aliquam in expedita, impedit quas ipsum nesciunt ipsa ullam
+//           consequuntur dignissimos numquam at nisi porro a, quaerat rem repellendus.
+//           Voluptates perspiciatis, in pariatur impedit, nam facilis libero dolorem
+//           dolores sunt inventore perferendis, aut sapiente modi nesciunt.
+//         </Typography>
+//       </Box>
+//     </Box>
+//   );
+// }
+
+// DrawerAppBar.propTypes = {
+//   /**
+//    * Injected by the documentation to work in an iframe.
+//    * You won't need it on your project.
+//    */
+//   window: PropTypes.func,
+// };
+
+// export default DrawerAppBar;
+
+export default function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          {HEADER_DATA.map((data) => (
-            <li key={data.path}>
-              <NavLink to={data.path}>{data.page}</NavLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    // <ThemeProvider theme={theme}>
+    <AppBar
+      component="nav"
+      sx={{ alignItems: "center", paddingY: 1, backgroundColor: "#032613" }}
+    >
+      <Toolbar sx={{ width: 1420 }}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Button sx={{ padding: 0 }}>
+            <img src={Logo} alt="" />
+          </Button>
+        </Box>
+        <ButtonGroup sx={{ gap: 1, mr: 1 }}>
+          <Button variant="text" color="inherit" sx={{ textTransform: "none" }}>
+            My Learning
+          </Button>
+          <Button variant="text" color="inherit" sx={{ textTransform: "none" }}>
+            Marketplace
+          </Button>
+          <Button variant="text" color="inherit" sx={{ textTransform: "none" }}>
+            Become a member
+          </Button>
+          <Button variant="text" color="inherit" sx={{ textTransform: "none" }}>
+            AgAI
+          </Button>
+          <Button variant="text" color="inherit" sx={{ textTransform: "none" }}>
+            Wishlist
+          </Button>
+          <Button variant="text" color="inherit" sx={{ textTransform: "none" }}>
+            Cart
+          </Button>
+        </ButtonGroup>
+        <Button
+          variant="contained"
+          color="inherit"
+          sx={{
+            textTransform: "none",
+            color: "black",
+            borderRadius: 100,
+          }}
+          startIcon={
+            <AccountCircleOutlinedIcon
+              sx={{
+                color: "black",
+                borderRadius: "100",
+                fontSize: 20,
+              }}
+            />
+          }
+        >
+          Login
+        </Button>
+      </Toolbar>
+      {/* <Box>
+        <Typography>faowifhjairhg0eoirhfgj0wepr9jgfaowifhjairhg0eoirhfg j0wepr9jgfaowifhjairhg0eoirhfgj0wepr9jgfaow ifhjairhg0eoirhfgj0wepr9jgfaowifhjairhg0eoirhfgj0wepr9jgfao wifhjairhg0eoirhfgj0wepr9jgfaowifhjairhg0eoirhfgj0wepr9jgfaowifhjairhg0eoirhfgj0wepr9 jgfaowifhjairhg0eoirhfgj0wepr9jgfaowifhjairhg0eo irhfgj0wepr9jgfaowifhjairhg0eoirhfgj0wepr9jgfaowifhjairhg0eoirhfgj0wepr9jgfaowifhjai rhg0eoirhfgj0wepr9jgfaowifhjairhg0eoirhfgj0wepr9jgfaowifhjairhg0eoirhfgj0wepr9jgfaowi fhjairhg0eoirhfgj0wepr9jgfaowifhjairhg0eoirhfgj0wepr9jgfaowifhjairhg0eoirhfgj0wepr9jg</Typography>
+      </Box> */}
+    </AppBar>
+    // </ThemeProvider>
   );
 }
-
-export default Navigation;
