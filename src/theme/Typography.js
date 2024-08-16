33e24482d544008dd740ab0a2d1ee0b6da2f17c5
PHOSDA <panhaphosda@gmail.com>
2024-08-16 14:57:20 +0700

    feat-theme: prepare typography, index.css

--- src/theme/Typography.js
+++ src/theme/Typography.js
@@ -6,79 +6,79 @@ const typography = {
   h1: {
     fontWeight: fontWeight.bold, // Bold
     fontSize: '45px', // Large heading
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   h2: {
     fontWeight: fontWeight.bold, // Bold
     fontSize: '40px', // Medium heading
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   h3: {
     fontWeight: fontWeight.semiBold, // SemiBold
     fontSize: '30px', // Small heading
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   h4: {
     fontWeight: fontWeight.semiBold, // SemiBold
     fontSize: '25px', // Smaller heading
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
 
 
   blgsm: {   // body large semibold
     fontWeight: fontWeight.medium, // Semibold
     fontSize: '20px', // Standard body text
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   blgmd: {  // body large medium
     fontWeight: fontWeight.medium, // Medium
     fontSize: '20px', // Standard body text
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   blgr : {  // body large regular
     fontWeight: fontWeight.regular, // Regular
     fontSize: '20px', // Small body text
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   bmdsm:{  // body mudium semibold
     fontWeight: fontWeight.medium, // Medium
     fontSize: '16px', // Captions or small notes
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   bmdmd: { // body medium medium
     fontWeight: fontWeight.regular, // Regular
     fontSize: '16px', // Very small text
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   bmdr: {  // body medium regular
     fontWeight: fontWeight.regular, // Regular
     fontSize: '16px', // Extra small text
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   bssm: {   // body small semibold
     fontWeight: fontWeight.regular, // Regular
     fontSize: '14px', // Extra small text
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
  bsr: {  // body small regular
     fontWeight: fontWeight.regular, // Regular
     fontSize: '14px', // Extra small text
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   bxsr: { // body extra small regular
     fontWeight: fontWeight.regular, // Regular
     fontSize: '12px', // Extra small text
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   bxsmd: { // body extra small meduim 
     fontWeight: fontWeight.regular, // Regular
     fontSize: '12px', // Extra small text
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   btr: { // body tiny regular
     fontWeight: fontWeight.regular, // Regular
     fontSize: '10px', // Extra small text
-    lineHeight: 'auto',
+    lineHeight: 'normal',
   },
   
 };
