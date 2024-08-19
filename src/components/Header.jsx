a62e1ff5e802440c631085235fcc85b4bd503663
sovanchitra <chivsovanchetra@gmail.com>
2024-08-19 13:14:48 +0700

    feat: implement agteach header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,23 @@
+import { Link as RouterLink } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import Button from '@mui/material/Button';
+import Link from "@mui/material/Link";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+// import AdbIcon from "@mui/icons-material/Adb";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+
+import Logo from "../assets/logo.png";
 
 const HEADER_DATA = [
   { page: "My Learning", path: "mylearning" },
@@ -8,23 +26,131 @@ const HEADER_DATA = [
   { page: "AgAI", path: "agai" },
   { page: "Wishlist", path: "wishlist" },
   { page: "Cart", path: "cart" },
-  { page: "Login", path: "login" },
+  // { page: "Login", path: "login" },
 ];
 
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
 function Navigation() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <header>
-      <nav>
-        <ul>
-          {HEADER_DATA.map((data) => (
-            <li key={data.path}>
-              <NavLink to={data.path}>{data.page}</NavLink>
-              <Button href={data.path}>{data.page}</Button>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
+
+          {/* logo */}
+          <Box sx={{ flexGrow: 1, display: "flex" }}>
+            <Link to="/">
+              <img src={Logo} alt="" />
+            </Link>
+          </Box>
+
+          {/* mobile */}
+          <Box sx={{ display: { xs: "block", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {HEADER_DATA.map((data) => (
+                <MenuItem key={data.path} onClick={handleCloseNavMenu}>
+                  <Link component={NavLink} to={data.path}>{data.page}</Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
+
+          {/* desktop */}
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            {HEADER_DATA.map((data) => (
+              <Button component={NavLink} variant="" to={data.path}>
+                {data.page}
+              </Button>
+            ))}
+            <Button
+              startIcon={<AccountCircleOutlinedIcon />}
+              variant="contained"
+            >
+              <RouterLink to="/login">Login</RouterLink>
+            </Button>
+          </Box>
+
+          {/* <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box> */}
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
