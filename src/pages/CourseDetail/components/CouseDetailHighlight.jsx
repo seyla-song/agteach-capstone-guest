3deb44ebbc16c7449575c45cfbbe9dd2c9591340
sela-bet <betsela555@gmail.com>
2024-09-01 15:29:28 +0700

    style: add color and typography to course highlight

--- src/pages/CourseDetail/components/CouseDetailHighlight.jsx
+++ src/pages/CourseDetail/components/CouseDetailHighlight.jsx
@@ -2,33 +2,55 @@ import { Grid, Stack, Typography } from '@mui/material';
 
 export const CourseDetailHighlight = () => {
   return (
-    <Grid border={'1px solid black'} item xs={12}>
-      <Grid container paddingY={10}>
+    <Grid item xs={12}>
+      <Grid container>
         <Grid xs={7} item>
-          <Typography>What you learn in this course? What included</Typography>
-          <Typography>
-            ✅ How to mix and manage nutrient solutions to ensure optimal plant
-            growth and health in a soil-free environment
-          </Typography>
-          <Typography>
-            ✅ How to design and build various types of hydroponic systems,
-            including nutrient film techniques and deep water culture.
-          </Typography>
-          <Typography>
-            ✅ Techniques for managing light, temperature, and humidity to
-            create the ideal conditions for indoor plant growth.
-          </Typography>
-        </Grid>
-        <Grid xs={5} item bgcolor={'yellow'}>
-          <Stack>
-            <Typography>This course include:</Typography>
-            <Stack direction="row" gap={2}>
-              <Typography>0</Typography>
-              <Typography variant="body1">30 hours</Typography>
+          <Stack
+            gap
+            justifyContent="center"
+            alignItems="center"
+            bgcolor="primary.dark"
+            color={'white'}
+            padding={10}
+          >
+            <Stack gap maxWidth={500}>
+              <Typography variant="h4">
+                What you learn in this course? What included
+              </Typography>
+              <Typography variant="bxsr">
+                ✅ How to mix and manage nutrient solutions to ensure optimal
+                plant growth and health in a soil-free environment
+              </Typography>
+              <Typography variant="bxsr">
+                ✅ How to design and build various types of hydroponic systems,
+                including nutrient film techniques and deep water culture.
+              </Typography>
+              <Typography variant="bxsr">
+                ✅ Techniques for managing light, temperature, and humidity to
+                create the ideal conditions for indoor plant growth.
+              </Typography>
             </Stack>
-            <Stack direction="row" gap={2}>
-              <Typography>0</Typography>
-              <Typography variant="body1">60 videos</Typography>
+          </Stack>
+        </Grid>
+        <Grid xs={5} item>
+          <Stack
+            color="primary.dark"
+            bgcolor="grey.300"
+            justifyContent="center"
+            alignItems="center"
+            height="100%"
+            gap
+          >
+            <Stack>
+              <Typography variant="h4">This course include:</Typography>
+              <Stack direction="row" gap={2}>
+                <Typography>0</Typography>
+                <Typography variant="bsr">30 hours</Typography>
+              </Stack>
+              <Stack direction="row" gap={2}>
+                <Typography>0</Typography>
+                <Typography variant="bsr">60 videos</Typography>
+              </Stack>
             </Stack>
           </Stack>
         </Grid>
@@ -36,3 +58,4 @@ export const CourseDetailHighlight = () => {
     </Grid>
   );
 };
+
