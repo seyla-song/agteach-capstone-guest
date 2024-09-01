5da1c380ef42fbf465c615268d6cc73c4f570f05
sela-bet <betsela555@gmail.com>
2024-09-01 17:50:00 +0700

    style: change rootlayout to default route

--- src/route/Route.js
+++ src/route/Route.js
@@ -22,8 +22,7 @@ import {
 export const router = createBrowserRouter([
   {
     path: '/',
-    // element: <RootLayout />,
-    element: <CourseDetailPage />,
+    element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
       { path: 'search', element: <SearchResultPage /> },
