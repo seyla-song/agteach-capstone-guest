44722df7280119db2e513440dbe92b9535f893d7
sovanchitra <chivsovanchetra@gmail.com>
2024-09-02 21:48:07 +0700

    feat-home-page: implement homepage component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { CssBaseline } from "@mui/material";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
