9f6e380040aed1efce3815506f79191b278ae9b2
PHOSDA <panhaphosda@gmail.com>
2024-09-03 17:17:39 +0700

    ref: reset App.js to default

--- src/App.js
+++ src/App.js
@@ -3,24 +3,12 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './route/Route';
 import { ThemeProvider } from '@emotion/react';
 import theme from './theme/theme';
-import SignUp from './pages/LoginSignup/SignUp';
-import Login from './pages/LoginSignup/LogIn';
-import PersonalInfoForm from './pages/LoginSignup/PersonalInformation';
-import ForgotPassword from './pages/LoginSignup/ForgotPassword';
-import ResetPassword from './pages/LoginSignup/ResetPassword';
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      {/* <RouterProvider router={router} /> */}
-      {/* <SignUp /> */}
-      {/* <Login /> */}
-      {/* <PersonalInfoForm /> */}
-      <ForgotPassword />
-      <ResetPassword/>
-
-
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
