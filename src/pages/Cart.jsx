9b4d15922afc369c0c8fb054021ae5ec021dd72c
sela-bet <betsela555@gmail.com>
2024-09-05 13:27:48 +0700

    init: initialize core component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,22 @@
+import { Box, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 function CartPage() {
   return (
     <>
-      <h1>Cart page</h1>
+      <Typography variant="h4">Your Cart</Typography>
+      <Typography variant="body1">Please review your cart before payment</Typography>
+      <Box
+        sx={{
+          height: 200,
+          width: 200,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img src="https://via.placeholder.com/200" alt="cart" />
+      </Box>
       <Link to='/payment'>Pay</Link>
     </>
   );
