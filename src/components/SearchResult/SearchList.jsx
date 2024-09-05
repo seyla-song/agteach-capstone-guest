284c291d6fb4343f685b5d686d8566e7c29ebebb
Pav Munyphalla <lalaphalla@gmail.com>
2024-09-05 13:40:07 +0700

    feat: add mobile filter form

---
+++ src/components/SearchResult/SearchList.jsx
@@ -0,0 +1,18 @@
+import { Box, Button, Grid2 as Grid, Stack } from "@mui/material";
+import { CustomCard } from "../CustomCard";
+
+function SearchList({ dataObj, cardVariant }) {
+  return (
+    <>
+      <Grid container size={{ xs: 12 }} width={"100%"}>
+        {dataObj.map((product, idx) => (
+          <Grid size={{ xs: 4 }}>
+            <CustomCard key={idx} dataObj={product} variant={cardVariant} />
+          </Grid>
+        ))}
+      </Grid>
+      <Button variant="outlined" fullWidth>View More</Button>
+    </>
+  );
+}
+export default SearchList;
