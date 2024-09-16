4089b615049c004f21a492a96f19ebebb6313076
Khit Khom <khomkhit460@gmail.com>
2024-09-16 12:12:38 +0700

    feat-auth-signup: add more layout and style for responsive in verification page.

--- src/pages/Verification.jsx
+++ src/pages/Verification.jsx
@@ -1,4 +1,12 @@
-import { Box, Typography, Button, Stack } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Button,
+  Stack,
+  Grid2,
+  responsiveFontSizes,
+  Grid,
+} from "@mui/material";
 import FormInput from "../components/LoginSignup/FormInput";
 import { useForm } from "react-hook-form";
 import { useVerifyEmailMutation } from "../services/api/verifySlice";
@@ -35,51 +43,70 @@ export default function VerificationPage() {
       }}
     >
       <LogoLink />
-      <Stack direction={"row"} alignItems={"center"} gap={2}>
-        <Stack>
-          <Box
-            component={"iframe"}
-            border={"none"}
-            src="https://lottie.host/embed/7e3e9b9a-bfc5-43b9-83b8-f9865bff7bf6/OkwRtcwUA1.json"
-          ></Box>
-          <Typography variant="h3">
-            Check your email for the verification code .
-          </Typography>
-          <Typography variant="bsr" sx={{ color: "dark.300" }}>
-            Please enter code number that you receive in the email.
-          </Typography>
-        </Stack>
-        <Stack gap={2}>
-          <Typography variant="blgsm">Enter verification code</Typography>
-          <Box component="form" onSubmit={handleSubmit(onSubmit)}>
-            <FormInput
-              mt={2}
-              label="Code"
-              type="text"
-              {...register("code")}
-              required
-            />
-            <Stack gap={1} pt={1}>
-              <Button
-                fullWidth
-                type="submit"
-                variant="contained"
-                disabled={isLoading}
-              >
-                Submit
-              </Button>
-              <Button
-                fullWidth
-                type="submit"
-                variant="outlined"
-                disabled={isLoading}
-                startIcon={<ArrowBack />}
-              >
-                Go Back
-              </Button>
-            </Stack>
+      <Grid2 container direction={"row"} alignItems={"center"} gap={2}>
+        <Grid2 item sx={{ width: { xs: "100%", sm: "55%" } }}>
+          <Stack objectFit={"contain"}>
+            <Box
+              height={{ sm: "430px", xs: "246px" }}
+              objectFit={"contain"}
+              component={"iframe"}
+              border={"none"}
+              src="https://lottie.host/embed/7e3e9b9a-bfc5-43b9-83b8-f9865bff7bf6/OkwRtcwUA1.json"
+            ></Box>
+          </Stack>
+          <Box textAlign={"center"}>
+            <Typography sx={{ typography: { xs: "h4", sm: "blgsm" } }}>
+              Check your email for verification code .
+            </Typography>
+            <Typography variant="bsr" sx={{ color: "dark.300" }}>
+              Please enter code number that you receive in the email.
+            </Typography>
           </Box>
-        </Stack>
+        </Grid2>
+        <Grid2 item gap={2} sx={{ width: { xs: "100%", sm: "40%" } }}>
+          <Box width={"100%"}>
+            <Typography pb={2} variant="blgsm">
+              Enter verification code
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+              <FormInput
+                mt={2}
+                label="Code"
+                type="text"
+                {...register("code")}
+                required
+              />
+              <Stack gap={1} pt={1}>
+                <Button
+                  fullWidth
+                  type="submit"
+                  variant="contained"
+                  disabled={isLoading}
+                >
+                  Submit
+                </Button>
+                <Button
+                  fullWidth
+                  type="submit"
+                  variant="contained"
+                  disabled={isLoading}
+                  color="secondary"
+                >
+                  Resend Code
+                </Button>
+                <Button
+                  fullWidth
+                  type="submit"
+                  variant="outlined"
+                  disabled={isLoading}
+                  startIcon={<ArrowBack />}
+                >
+                  Go Back
+                </Button>
+              </Stack>
+            </Box>
+          </Box>
+        </Grid2>
         {isSuccess && (
           <Typography color="success.main">Verification successful!</Typography>
         )}
@@ -88,7 +115,7 @@ export default function VerificationPage() {
             {error?.data?.message || "Verification failed!"}
           </Typography>
         )}
-      </Stack>
+      </Grid2>
     </Box>
   );
 }
