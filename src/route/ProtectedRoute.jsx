9fde122c4249ffc58cae53f621d9df69d9945f17
sovanchitra <chivsovanchetra@gmail.com>
2024-09-19 11:28:17 +0700

    feat-isLogin: modify url request for isLogin

--- src/route/ProtectedRoute.jsx
+++ src/route/ProtectedRoute.jsx
@@ -1,9 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NotLoginStateComponent from "../components/LoginPromptComponent";
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { useIsLoginQuery } from "../services/api/authSlice";
-import { checkLoginStatus } from "../store/slices/authSlice";
 
 /**
  * A protected route component that conditionally renders an Outlet or a NotLoginStateComponent based on the user's authentication status.
@@ -13,12 +10,6 @@ import { checkLoginStatus } from "../store/slices/authSlice";
  */
 
 export default function ProtectedRoute({ pageName }) {
-  const dispatch = useDispatch();
-  const response = useIsLoginQuery();
-  console.log(response);
-  
-  
-  // dispatch(checkLoginStatus(isSuccess));
   const { isAuthenticated } = useSelector((state) => state.auth);
 
   return isAuthenticated ? (
