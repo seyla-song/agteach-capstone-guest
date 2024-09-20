b51f75ee8435408d10dcc5b83da570814543a5c9
sovanchitra <chivsovanchetra@gmail.com>
2024-09-20 14:24:44 +0700

    fix: change local to cloud endpoint

--- src/services/api/userApi.js
+++ src/services/api/userApi.js
@@ -1,25 +1,3 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-// export const userApi = createApi({
-//   reducerPath: "api",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: "http://localhost:3001",
-//     credentials: "include",
-//   }),
-
-//   endpoints: (builder) => ({
-//     updatePassword: builder.mutation({
-//       query: () => ({
-//         url: "/api/users/updatePassword",
-//         method: "PATCH",
-//       }),
-//     }),
-
-//   }),
-// });
-
-// export const { useUpdatePasswordMutation } = userApi;
-
 //  https://api.agteach.site
 //  http://localhost:3001
 
@@ -28,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3001",
+    baseUrl: "https://api.agteach.site",
     credentials: "include",
   }),
   endpoints: (builder) => ({
