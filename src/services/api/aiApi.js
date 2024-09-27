b1b3e23b2fa47ae45616aec96eeab611f207d479
sela-bet <betsela555@gmail.com>
2024-09-27 19:57:29 +0700

    chore: add https to baseurl to aiApi

--- src/services/api/aiApi.js
+++ src/services/api/aiApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const aiApi = createApi({
   reducerPath: 'aiApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://ai.agteach.site',
+    baseUrl: 'https://ai.agteach.site',
   }),
   endpoints: (builder) => ({
     predictImage: builder.mutation({
