e35c7df23b0b82b8e9d822747f9b71a43607be4d
sela-bet <betsela555@gmail.com>
2024-10-08 19:59:48 +0700

    ref: add payment success and fail to route

--- src/route/Route.js
+++ src/route/Route.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 import {
   HomePage,
   SearchResultPage,
@@ -22,65 +22,75 @@ import {
   ErrorPage,
   LoginPromptComponent,
   VerificationPage,
-} from "./index";
+  SuccessPayment,
+  FailPayment,
+} from './index';
 
-import ProtectedRoute from "./ProtectedRoute";
+import ProtectedRoute from './ProtectedRoute';
 
-import AuthRootLayout from "./AuthRoot";
+import AuthRootLayout from './AuthRoot';
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
 
     children: [
       { index: true, element: <HomePage /> },
-      { path: "search", element: <SearchResultPage /> },
+      { path: 'search', element: <SearchResultPage /> },
       {
         path: 'cart',
-        element: <ProtectedRoute pageName="cart" />, 
+        element: <ProtectedRoute pageName="cart" />,
         children: [{ index: true, element: <CartPage /> }],
       },
       {
         path: 'mylearning',
-        element: <ProtectedRoute pageName="course" />, 
+        element: <ProtectedRoute pageName="course" />,
         children: [{ index: true, element: <MyLearningPage /> }],
       },
-      { path: "agai", element: <AgAiPage /> },
+      { path: 'agai', element: <AgAiPage /> },
       {
         path: 'wishlist',
-        element: <ProtectedRoute pageName="wishlist" />, 
+        element: <ProtectedRoute pageName="wishlist" />,
         children: [{ index: true, element: <WishlistPage /> }],
       },
-      { path: "cart", element: <CartPage /> },
-      { path: "mylearning", element: <MyLearningPage /> },
-      { path: "agai", element: <AgAiPage /> },
-      { path: "wishlist", element: <WishlistPage /> },
-      { path: "guest-profile", element: <GuestProfilePage /> },
-      { path: "instructor-profile", element: <InstructorProfilePage /> },
-      { path: "courses/:coursesId", element: <CourseDetailPage /> },
-      { path: "marketplace", element: <MarketPlacePage /> },
-      { path: "marketplace/:productId", element: <ProductDetailPage /> },
-      { path: "not-login", element: <LoginPromptComponent /> },
-      { path: "*", element: <ErrorPage /> },
+      { path: 'cart', element: <CartPage /> },
+      { path: 'mylearning', element: <MyLearningPage /> },
+      { path: 'agai', element: <AgAiPage /> },
+      { path: 'wishlist', element: <WishlistPage /> },
+      { path: 'guest-profile', element: <GuestProfilePage /> },
+      { path: 'instructor-profile', element: <InstructorProfilePage /> },
+      { path: 'courses/:coursesId', element: <CourseDetailPage /> },
+      { path: 'marketplace', element: <MarketPlacePage /> },
+      { path: 'marketplace/:productId', element: <ProductDetailPage /> },
+      { path: 'not-login', element: <LoginPromptComponent /> },
+      { path: '*', element: <ErrorPage /> },
     ],
   },
   {
-    path: "courses/:coursesId/watch/:videoId",
+    path: 'courses/:coursesId/watch/:videoId',
     element: <CourseVideoPage />,
   },
   {
-    path: "auth/",
+    path: 'success-payment',
+    element: <SuccessPayment />,
+  },
+  {
+    path: 'fail-payment',
+    element: <FailPayment />,
+  },
+  {
+    path: 'auth/',
     element: <AuthRootLayout />,
     children: [
-      { path: "signup", element: <SignupPage /> },
-      { path: "signup/info", element: <PersonalInformationPage /> },
-      { path: "signup/verification", element: <VerificationPage /> },
-      { path: "login", element: <LoginPage /> },
-      { path: "info", element: <PersonalInformationPage /> },
-      { path: "forgot-password", element: <ForgotPasswordPage /> },
-      { path: "reset-password/:resetToken", element: <ResetPasswordPage /> },
+      { path: 'signup', element: <SignupPage /> },
+      { path: 'signup/info', element: <PersonalInformationPage /> },
+      { path: 'signup/verification', element: <VerificationPage /> },
+      { path: 'login', element: <LoginPage /> },
+      { path: 'info', element: <PersonalInformationPage /> },
+      { path: 'forgot-password', element: <ForgotPasswordPage /> },
+      { path: 'reset-password/:resetToken', element: <ResetPasswordPage /> },
     ],
   },
-  { path: "payment", element: <PaymentPage /> },
+  { path: 'payment', element: <PaymentPage /> },
 ]);
