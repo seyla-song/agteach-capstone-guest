1b7225f95060e913233b6df29b3d1be7ff6852dd
sela-bet <betsela555@gmail.com>
2024-10-14 13:40:13 +0700

    chore: add Temp file for testing cart logic

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,11 @@
+import { Box, Container, Typography } from '@mui/material';
+
+export const ProductList = () => {
+  return (
+    <Container>
+      <Box>
+        <Typography>Product List</Typography>
+      </Box>
+    </Container>
+  );
+};
