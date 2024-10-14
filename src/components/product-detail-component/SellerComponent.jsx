6af7e7f1e7a3d52a4b3bdc55c71a57d6bf4ddb78
sela-bet <betsela555@gmail.com>
2024-10-14 21:12:11 +0700

    ref: remove favourite icon from SellerComponent

--- src/components/product-detail-component/SellerComponent.jsx
+++ src/components/product-detail-component/SellerComponent.jsx
@@ -1,15 +1,6 @@
-import {
-  Avatar,
-  Box,
-  Divider,
-  IconButton,
-  Stack,
-  Typography,
-} from "@mui/material";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import { Avatar, Box, Divider, Stack, Typography } from '@mui/material';
+
+import { useNavigate } from 'react-router';
 
 /**
  * SellerComponent renders a component that displays seller information
@@ -17,7 +8,6 @@ import { useNavigate } from "react-router";
  * @returns {React.ReactElement} A JSX element that renders a seller component
  */
 export default function SellerComponent({ seller }) {
-  const [wishlist, setWishlist] = useState(false);
   const navigate = useNavigate();
 
   const goToInstructorProfile = (id) => navigate(`/instructor-profile/${id}`);
@@ -27,33 +17,32 @@ export default function SellerComponent({ seller }) {
    *
    * @return {JSX.Element} The JSX element representing the seller component
    */
-  const handleWishlistButton = () => {
-    setWishlist((wishlist) => !wishlist);
-  };
-  
   return (
     <Stack spacing={1.5}>
       <Divider />
       <Box
-        sx={{ display: "flex", gap: "20px", justifyContent: "space-between" }}
+        sx={{ display: 'flex', gap: '20px', justifyContent: 'space-between' }}
       >
         <Stack direction="row" alignItems="center">
-          <Avatar src={seller.image_url} onClick={() => goToInstructorProfile(seller.instructorId)}/>
+          <Avatar
+            src={seller.image_url}
+            onClick={() => goToInstructorProfile(seller.instructorId)}
+          />
           <Stack direction="column" sx={{ marginLeft: 2 }}>
-            <Typography variant="bmdsm" onClick={() => goToInstructorProfile(seller.instructorId)}>{seller.firstName + " " + seller.lastName}</Typography>
+            <Typography
+              variant="bmdsm"
+              onClick={() => goToInstructorProfile(seller.instructorId)}
+            >
+              {seller.firstName + ' ' + seller.lastName}
+            </Typography>
             <Typography variant="bxsr" color="dark.200">
               {/* will fix later */}
               {'Kep, Kampot, Cambodia'}
             </Typography>
           </Stack>
         </Stack>
-
-        <IconButton onClick={handleWishlistButton}>
-          {wishlist ? <FavoriteIcon color="red" /> : <FavoriteBorderIcon />}
-        </IconButton>
       </Box>
       <Divider />
-
     </Stack>
   );
 }
