f83681c5eb64ffe24616b038ad5f508af11bc357
sela-bet <betsela555@gmail.com>
2024-10-17 11:52:24 +0700

    chore: create a fail payment page

--- src/pages/FailPayment.jsx
+++ src/pages/FailPayment.jsx
@@ -1,9 +1,72 @@
-import { Box, Typography } from '@mui/material';
+import React from 'react';
+import { Box, Typography, Grid, Stack, Button, Link } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 export default function FailPayment() {
   return (
-    <Box>
-      <Typography>Your payment is Fail</Typography>
+    <Box
+      bgcolor="grey.100"
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Grid container maxWidth="lg" sx={{ padding: 4 }} bgcolor="white">
+        <Grid
+          item
+          xs={12}
+          sx={{
+            padding: 5,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <HighlightOffIcon sx={{ fontSize: 60, color: 'red.main' }} />
+
+          <Typography color="red.main" variant="blgsm" mt={2}>
+            We have not received your payment
+          </Typography>
+          <Typography variant="bsr" textAlign="center" color="dark.200" mt>
+            You payment did not complete, try again
+          </Typography>
+          <Box pt={3}>
+            <Link component={RouterLink} to="/" underline="none">
+              <Button variant="outlined" sx={{ width: 400 }}>
+                Back to AgTeach
+              </Button>
+            </Link>
+          </Box>
+
+          <Stack direction="row" gap pt={2}>
+            <Typography variant="bxsmd">POWERED BY STRIPE</Typography>
+            <Typography variant="bxsmd">|</Typography>
+            <Typography variant="bxsr">
+              <Box
+                component="a"
+                href="#"
+                color="dark.200"
+                sx={{ textDecoration: 'none' }}
+              >
+                Terms and Conditon
+              </Box>
+            </Typography>
+            <Typography variant="bxsr">
+              <Box
+                component="a"
+                href="#"
+                color="dark.200"
+                sx={{ textDecoration: 'none' }}
+              >
+                Privacy Policy
+              </Box>
+            </Typography>
+          </Stack>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
