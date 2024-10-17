146120f204862192f91daff0aad881f4031a4a42
sela-bet <betsela555@gmail.com>
2024-10-17 21:25:23 +0700

    chore: remove console log from CarouselComponent

--- src/components/HomeComponent/CarouselComponent.jsx
+++ src/components/HomeComponent/CarouselComponent.jsx
@@ -3,8 +3,6 @@ import { CustomCarousel } from "../CustomCarousel";
 import { products, courses } from "../../utils/carouselDummy";
 
 export default function CarouselComponent({ data, cardVariant, children }) {
-  // const carouselData = data === "products" ? products : courses;
-  console.log(data);
   return (
     <Box sx={{ p: { xs: "10px", md: 0 } }}>
       <Typography
