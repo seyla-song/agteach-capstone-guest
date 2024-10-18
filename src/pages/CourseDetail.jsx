8648649835a6a74490110e89d6c3012da9c9b0ba
sela-bet <betsela555@gmail.com>
2024-10-18 14:39:35 +0700

    chore: remove unused shuffle and condition

--- src/pages/CourseDetail.jsx
+++ src/pages/CourseDetail.jsx
@@ -1,6 +1,5 @@
 import { Divider, Grid, Stack } from '@mui/material';
 import {
-  CourseDetailHero,
   CourseDetailHighlight,
   CourseDetailContent,
 } from '../components/CourseDetail/index';
@@ -10,21 +9,11 @@ import {
   useGetOneCourseQuery,
 } from '../services/api/courseApi';
 import { SuggestedCourseProduct } from '../components/SuggestCourseProduct';
-import { useEffect, useState, Fragment } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
-function shuffle(array) {
-  const newArray = [...array]; // Create a copy of the array
-
-  for (let i = newArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [newArray[i], newArray[j]] = [newArray[j], newArray[i]]; // Swap elements
-  }
-
-  return newArray; // Return the shuffled copy
-}
 
 const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
 
@@ -42,63 +31,22 @@ function CourseDetailPage() {
     useGetRecommendedCoursesQuery(coursesId);
 
   useEffect(() => {
-
     if (recommendedCoursesData) {
-      setRecommendedCourses(shuffle(recommendedCoursesData.data));
+      setRecommendedCourses(recommendedCoursesData.data);
     }
 
     window.scrollTo(0, 0);
   }, [recommendedCoursesData, currentCourseData]);
 
-  if (isCurrentCourseDataLoading)
-    return (
-      <div
-        style={{
-          width: '100%',
-          height: 'calc(100vh - 68px)',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        Loading...
-      </div>
-    );
-  if (isCurrentCourseError)
-    return (
-      <div
-        style={{
-          width: '100%',
-          height: 'calc(100vh - 68px)',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        Error: {currentCoursesError}
-      </div>
-    );
-  if (!currentCourseData)
-    return (
-      <div
-        style={{
-          width: '100%',
-          height: 'calc(100vh - 68px)',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        Course Not Found.
-      </div>
-    );
+  if (isCurrentCourseDataLoading) return <div>Loading...</div>;
+  if (isCurrentCourseError) return <div>Error: {currentCoursesError}</div>;
 
   return (
     <Elements stripe={stripePromise}>
       <Stack alignItems="center">
         <Stack width="100%" alignItems="center" bgcolor={'primary.dark'}>
           <Grid sx={{ maxWidth: '1420px' }} container paddingX={1}>
-            <CourseDetailHero courseData={currentCourseData?.data} />
+            {/* <CourseDetailHero courseData={currentCourseData?.data} /> */}
           </Grid>
         </Stack>
         <Grid sx={{ maxWidth: '1420px' }} container paddingX={1}>
