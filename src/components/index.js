a54a4665a034faea1a498e306487145ef1fd6996
sela-bet <betsela555@gmail.com>
2024-10-21 15:07:48 +0700

    chore: refractor import in verification

--- src/components/index.js
+++ src/components/index.js
@@ -23,3 +23,6 @@ export { ProductWishlist } from './Wishlist/ProductWishlist';
 export { CourseWishlist } from './Wishlist/CourseWishlist';
 export { CategoryFilter } from './SearchResult/CategoryFilter';
 export { FilterByOther } from './SearchResult/FilterByOther';
+export { FormInput } from './LoginSignup/FormInput';
+export { LogoLink } from './LoginSignup/LogoLink';
+export { ResendCodeButton } from './LoginSignup/ResendCodeButton';
--- src/pages/Verification.jsx
+++ src/pages/Verification.jsx
@@ -1,15 +1,18 @@
-import { Box, Typography, Button, Stack, Grid2 } from "@mui/material";
-import FormInput from "../components/LoginSignup/FormInput";
-import { useForm } from "react-hook-form";
-import { useVerifyEmailMutation } from "../services/api/authApi";
-import LogoLink from "../components/LoginSignup/LogoLink";
-import { ArrowBack } from "@mui/icons-material";
-import { CustomAlert } from "../components/CustomAlert";
-import ResendCodeButton from "../components/LoginSignup/ResendCodeButton";
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useVerifyEmailMutation } from '../services/api/authApi';
 
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useState } from "react";
+import { Box, Typography, Button, Stack, Grid2 } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
+
+import {
+  CustomAlert,
+  FormInput,
+  LogoLink,
+  ResendCodeButton,
+} from '../components/index';
 
 export default function VerificationPage() {
   const [open, setOpen] = useState(true);
@@ -29,9 +32,9 @@ export default function VerificationPage() {
   const onSubmit = async (data) => {
     try {
       const response = await verifyEmail(data.emailVerifyCode).unwrap();
-      navigate("/");
+      navigate('/');
     } catch (err) {
-      console.error("Verification failed", err);
+      console.error('Verification failed', err);
     }
   };
 
@@ -45,37 +48,37 @@ export default function VerificationPage() {
   return (
     <Box
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: { sm: "80vh", xs: "100vh" },
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: { sm: '80vh', xs: '100vh' },
         paddingX: 5,
       }}
     >
       <LogoLink />
-      <Grid2 container direction={"row"} alignItems={"center"} gap={2}>
-        <Grid2 item="true" sx={{ width: { xs: "100%", sm: "55%" } }}>
+      <Grid2 container direction={'row'} alignItems={'center'} gap={2}>
+        <Grid2 item="true" sx={{ width: { xs: '100%', sm: '55%' } }}>
           <Stack>
             <Box
-              height={{ sm: "430px", xs: "246px" }}
-              objectfit={"contain"}
-              component={"iframe"}
-              border={"none"}
+              height={{ sm: '430px', xs: '246px' }}
+              objectfit={'contain'}
+              component={'iframe'}
+              border={'none'}
               src="https://lottie.host/embed/7e3e9b9a-bfc5-43b9-83b8-f9865bff7bf6/OkwRtcwUA1.json"
             ></Box>
           </Stack>
-          <Box textAlign={"center"}>
-            <Typography sx={{ typography: { xs: "h4", sm: "blgsm" } }}>
+          <Box textAlign={'center'}>
+            <Typography sx={{ typography: { xs: 'h4', sm: 'blgsm' } }}>
               Check your email for verification code .
             </Typography>
-            <Typography variant="bsr" sx={{ color: "dark.300" }}>
+            <Typography variant="bsr" sx={{ color: 'dark.300' }}>
               Please enter code number that you receive in the email.
             </Typography>
           </Box>
         </Grid2>
-        <Grid2 item="true" gap={2} sx={{ width: { xs: "100%", sm: "40%" } }}>
-          <Box width={"100%"}>
+        <Grid2 item="true" gap={2} sx={{ width: { xs: '100%', sm: '40%' } }}>
+          <Box width={'100%'}>
             <Typography pb={2} variant="blgsm">
               Enter verification code
             </Typography>
@@ -88,8 +91,8 @@ export default function VerificationPage() {
                 mt={2}
                 label="Code"
                 type="text"
-                {...register("emailVerifyCode", {
-                  required: "Verification code is required",
+                {...register('emailVerifyCode', {
+                  required: 'Verification code is required',
                 })}
               />
               {errors.code && (
@@ -126,7 +129,7 @@ export default function VerificationPage() {
                   variant="outlined"
                   disabled={isLoading}
                   startIcon={<ArrowBack />}
-                  onClick={() => navigate("/auth/signup")}
+                  onClick={() => navigate('/auth/signup')}
                 >
                   Go Back
                 </Button>
