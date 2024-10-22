0c0ebd73b4b8d64a447a5bedabff5ca48690867d
sela-bet <betsela555@gmail.com>
2024-10-22 12:02:23 +0700

    chore: remove unused import

--- src/components/MaketPlace/SortBy.jsx
+++ src/components/MaketPlace/SortBy.jsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
