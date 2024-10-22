7b1601b9e0940293dcfb6112d49903547caa34d8
sela-bet <betsela555@gmail.com>
2024-10-22 11:49:13 +0700

    ref: move style to bottom

--- src/components/MaketPlace/Category.jsx
+++ src/components/MaketPlace/Category.jsx
@@ -1,27 +1,5 @@
 import { Box, Chip, Typography } from '@mui/material';
 
-const categoryStyle = {
-  backgroundColor: 'dark.100',
-  color: 'dark.200',
-  border: (theme) => `2px solid ${theme.palette.grey[500]}`,
-  typography: { xs: 'bsr', sm: 'bmdsm' },
-  '&:hover': {
-    backgroundColor: 'primary.main', // New background color on hover
-    color: 'common.white', // New text color on hover
-    border: (theme) => `2px solid ${theme.palette.primary.main}`,
-  },
-};
-const categoryStyle1 = {
-  backgroundColor: 'primary.main',
-  color: 'common.white',
-  border: (theme) => `2px solid ${theme.palette.primary.main}`,
-  typography: { xs: 'bsr', sm: 'bmdsm' },
-  '&:hover': {
-    backgroundColor: 'primary.main', // New background color on hover
-    color: 'common.white', // New text color on hover
-    border: (theme) => `2px solid ${theme.palette.primary.main}`,
-  },
-};
 export const Category = ({ category, handleChange }) => {
   return (
     <Box sx={{ pr: 1 }}>
@@ -61,3 +39,26 @@ export const Category = ({ category, handleChange }) => {
   );
 };
 export default Category;
+
+const categoryStyle = {
+  backgroundColor: 'dark.100',
+  color: 'dark.200',
+  border: (theme) => `2px solid ${theme.palette.grey[400]}`,
+  typography: { xs: 'bsr', sm: 'bmdsm' },
+  '&:hover': {
+    backgroundColor: 'primary.main', // New background color on hover
+    color: 'common.white', // New text color on hover
+    border: (theme) => `2px solid ${theme.palette.primary.main}`,
+  },
+};
+const categoryStyle1 = {
+  backgroundColor: 'primary.main',
+  color: 'common.white',
+  border: (theme) => `2px solid ${theme.palette.primary.main}`,
+  typography: { xs: 'bsr', sm: 'bmdsm' },
+  '&:hover': {
+    backgroundColor: 'primary.main', // New background color on hover
+    color: 'common.white', // New text color on hover
+    border: (theme) => `2px solid ${theme.palette.primary.main}`,
+  },
+};
