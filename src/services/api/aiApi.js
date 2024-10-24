dbf156211f579ff9389126c35fd9f1ad6083c7e4
sela-bet <betsela555@gmail.com>
2024-10-24 13:04:10 +0700

    chore: move api to constant

--- src/services/api/aiApi.js
+++ src/services/api/aiApi.js
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { API_BASE_URL } from '../../constants/apiConstants';
+import { AI_API_URL } from '../../constants/apiConstants';
 
 export const aiApi = createApi({
   reducerPath: 'aiApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://ai.agteach.site',
+    baseUrl: AI_API_URL,
   }),
   endpoints: (builder) => ({
     predictImage: builder.mutation({
