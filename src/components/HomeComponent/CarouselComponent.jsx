90468da7223d16799c99720b2cfb895c328eca13
sela-bet <betsela555@gmail.com>
2024-10-25 14:13:05 +0700

    chore: remove unused declaration

--- src/components/HomeComponent/CarouselComponent.jsx
+++ src/components/HomeComponent/CarouselComponent.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { CustomCarousel } from '../CustomCarousel';
 
 export const CarouselComponent = ({ data, cardVariant, children }) => {
