7a44e66a42cbbd6b5bcd643049ad0c7a2355730c
sela-bet <betsela555@gmail.com>
2024-11-26 14:34:48 +0700

    feat: add link for view agai suggested course

--- src/components/AgAi/DiseaseInfoComponent.jsx
+++ src/components/AgAi/DiseaseInfoComponent.jsx
@@ -1,10 +1,21 @@
-import { List, ListItem, Stack, Typography } from '@mui/material';
-import WarningIcon from '@mui/icons-material/Warning';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import {
+  Button,
+  List,
+  ListItem,
+  Stack,
+  Typography,
+  Link,
+  Divider,
+  Alert,
+} from "@mui/material";
+import WarningIcon from "@mui/icons-material/Warning";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import BloodtypeIcon from "@mui/icons-material/Bloodtype";
+import { Link as RouterLink } from "react-router-dom";
 
-export const DiseaseInfoComponent = ({data}) => {
-
+export const DiseaseInfoComponent = ({ data }) => {
   const { cure, disease, problem, symptom } = data;
 
   return (
@@ -27,6 +38,26 @@ export const DiseaseInfoComponent = ({data}) => {
           icon={<FavoriteBorderOutlinedIcon color="teal" fontSize="large" />}
         />
       </Stack>
+      <Divider />
+      <Alert icon={<BloodtypeIcon />} severity="success">
+        <Stack gap={1}>
+          <Typography>Are you interested in {disease} ?</Typography>
+          <Link
+            component={RouterLink}
+            to="https://alphabeez.anbschool.org/courses/912"
+            target="_blank"
+            underline="none"
+          >
+            <Button
+              variant="contained"
+              color="secondary"
+              endIcon={<OndemandVideoIcon />}
+            >
+              Learn more on plant disease and prventions
+            </Button>
+          </Link>
+        </Stack>
+      </Alert>
     </Stack>
   );
 };
@@ -38,8 +69,8 @@ const InformationDetail = ({ title, desc, icon }) => {
         {icon}
         <Typography variant="h4">{title}</Typography>
       </Stack>
-      <List sx={{ listStyleType: 'disc', listStylePosition: 'inside' }}>
-        <ListItem sx={{ display: 'list-item', paddingY: 1 }}>{desc}</ListItem>
+      <List sx={{ listStyleType: "disc", listStylePosition: "inside" }}>
+        <ListItem sx={{ display: "list-item", paddingY: 1 }}>{desc}</ListItem>
       </List>
     </Stack>
   );
