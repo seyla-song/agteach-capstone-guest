a4b4874aa13e5732e601f3edde5583947b278019
sela-bet <betsela555@gmail.com>
2024-11-27 20:59:27 +0700

    feat: add translation for highlight

--- src/utils/displayDuration.js
+++ src/utils/displayDuration.js
@@ -1,25 +1,34 @@
-const displayDuration = ({ hours = 0, minutes = 0, seconds = 0 }) => {
-  let result = '';
+const displayDuration = ({
+  hours,
+  minutes,
+  seconds,
+  hourLabel = "",
+  minuteLabel = "d",
+  secondLabel = "",
+}) => {
+  let result = "";
+
+  // console.log({ hours, minutes, seconds });
 
   // Handle hours
   if (hours > 0) {
-    result += `${hours} hour${hours !== 1 ? 's' : ''}`;
+    result += `${hours} hour$`;
   }
 
   // Handle minutes
   if (minutes > 0) {
-    if (result) result += ', '; // Add comma if hours are present
-    result += `${minutes} minute${minutes !== 1 ? 's' : ''}`;
+    if (result) result += ", "; // Add comma if hours are present
+    result += `${minutes} ${minuteLabel}`;
   }
 
   // Handle seconds
   if (seconds > 0) {
-    if (result) result += ', '; // Add "and" if hours or minutes are present
-    result += `${seconds} second${seconds !== 1 ? 's' : ''}`;
+    if (result) result += ", "; // Add "and" if hours or minutes are present
+    result += `${seconds} ${secondLabel}`;
   }
 
   // Return the result or '0 seconds' if no duration is provided
-  return result || '0 seconds';
+  return result || `0 ${secondLabel}`;
 };
 
 export default displayDuration;
